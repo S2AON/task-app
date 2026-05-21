666b4b1646e54badf4334af7be75da1fa8a242ef
Addington Ramsey <addington.ramsey@gmail.com>
2026-05-21 00:10:57 -0500

    chore: initial migration with base schema

---
+++ sql/001_InitialCreate.sql
@@ -0,0 +1,108 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    IF SCHEMA_ID(N'Core') IS NULL EXEC(N'CREATE SCHEMA [Core];');
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    IF SCHEMA_ID(N'Auth') IS NULL EXEC(N'CREATE SCHEMA [Auth];');
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Auth].[User] (
+        [Id] uniqueidentifier NOT NULL,
+        [Email] nvarchar(max) NOT NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [GoogleId] nvarchar(max) NULL,
+        [FullName] nvarchar(max) NOT NULL,
+        [IsActive] bit NOT NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        [UpdatedAt] datetime2 NULL,
+        [CreatedBy] uniqueidentifier NULL,
+        [UpdatedBy] uniqueidentifier NULL,
+        [IsDeleted] bit NOT NULL,
+        CONSTRAINT [PK_User] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Core].[Task] (
+        [Id] uniqueidentifier NOT NULL,
+        [Title] nvarchar(200) NOT NULL,
+        [Description] nvarchar(1000) NOT NULL,
+        [DueDate] datetime2 NOT NULL,
+        [Status] int NOT NULL,
+        [AssignedTo] uniqueidentifier NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        [UpdatedAt] datetime2 NULL,
+        [CreatedBy] uniqueidentifier NULL,
+        [UpdatedBy] uniqueidentifier NULL,
+        [IsDeleted] bit NOT NULL DEFAULT CAST(0 AS bit),
+        CONSTRAINT [PK_Task] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Task_User_AssignedTo] FOREIGN KEY ([AssignedTo]) REFERENCES [Auth].[User] ([Id]) ON DELETE SET NULL,
+        CONSTRAINT [FK_Task_User_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [Auth].[User] ([Id]) ON DELETE NO ACTION
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Task_AssignedTo] ON [Core].[Task] ([AssignedTo]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    CREATE INDEX [IX_Task_CreatedBy] ON [Core].[Task] ([CreatedBy]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260521041639_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260521041639_InitialCreate', N'8.0.0');
+END;
+GO
+
+COMMIT;
+GO
+
