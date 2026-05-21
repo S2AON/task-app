c9d2bf2a5fd37528ab3b383f3b470355952b9c43
Addington Ramsey <addington.ramsey@gmail.com>
2026-05-21 00:11:32 -0500

    chore: reverse json camelcasing forcing on response

--- api/src/TaskApp.Api/Program.cs
+++ api/src/TaskApp.Api/Program.cs
@@ -1,4 +1,3 @@
-using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,11 +16,6 @@ var host = new HostBuilder()
     .ConfigureAppConfiguration(config => { config.AddEnvironmentVariables(); })
     .ConfigureServices((context, services) =>
     {
-        services.ConfigureHttpJsonOptions(options =>
-        {
-            options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-            options.SerializerOptions.PropertyNameCaseInsensitive = true;
-        });
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
 
